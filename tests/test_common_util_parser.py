c4e1790a9a5059b9db4df3c2b8140895d1c80f30
ariwk <adam.ruberti@sbb.ch>
2024-10-13 20:28:36 +0200

    chore: add tox

---
+++ tests/test_common_util_parser.py
@@ -0,0 +1,9 @@
+"""Tests python_polarion_utils.common"""
+
+from python_polarion_utils.common import util_argparse
+
+
+def test_get_script_arguments():
+    host = "http://localhost/polarion"
+    args = util_argparse.get_script_arguments(host)
+    assert args.app_url == host
