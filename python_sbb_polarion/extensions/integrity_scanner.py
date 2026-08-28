4b575590c2f2a393a090c79bfe6a11f2daddfea9
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2026-08-28 18:03:28 +0200

    feat: add settings API to integrity scanner
    The extension serves the generic /api/settings endpoints, so the client
    must expose them through PolarionGenericExtensionSettingsApi.

--- python_sbb_polarion/extensions/integrity_scanner.py
+++ python_sbb_polarion/extensions/integrity_scanner.py
@@ -1,10 +1,10 @@
 """Integrity Scanner Polarion Extension API"""
 
-from python_sbb_polarion.core.base import PolarionGenericExtensionApi, PolarionRestApiConnection
+from python_sbb_polarion.core.base import PolarionGenericExtensionApi, PolarionGenericExtensionSettingsApi, PolarionRestApiConnection
 from python_sbb_polarion.extensions._integrity_scanner import IntegrityScannerMixin
 
 
-class PolarionIntegrityScannerApi(IntegrityScannerMixin, PolarionGenericExtensionApi):
+class PolarionIntegrityScannerApi(IntegrityScannerMixin, PolarionGenericExtensionApi, PolarionGenericExtensionSettingsApi):
     """Integrity Scanner Polarion Extension API"""
 
     def __init__(self, polarion_connection: PolarionRestApiConnection) -> None:
